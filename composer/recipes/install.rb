dd1cf608d609259b7fd7c0a38edf5c26a64e85a5
Ben Lippincott <benji.lippincott@gmail.com>
2015-05-14 16:29:23 -0400

    Added a server subdir

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -6,7 +6,7 @@ node[:deploy].each do |application, deploy|
   script "install_composer" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current"
+    cwd "#{deploy[:deploy_to]}/current/server"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
     php composer.phar install --no-dev --no-interaction --optimize-autoloader
