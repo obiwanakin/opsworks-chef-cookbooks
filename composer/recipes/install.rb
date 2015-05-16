3a0bd955cb1e2a1a118aba56201033bb442db4f9
Ben Lippincott <benji.lippincott@gmail.com>
2015-05-16 12:31:11 -0400

    PUSH IT, PUSH IT

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -3,7 +3,7 @@
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
 node[:deploy].each do |application, deploy|
-  script "install_composer" do
+  script "install_deps" do
     interpreter "bash"
     user "root"
     cwd "#{deploy[:deploy_to]}/current/server"
